92e607968765fd83c84b49a10674544f6a9b7119
jesusMAes <jesusmarmolesp@gmail.com>
2022-08-31 13:14:35 +0200

    initialized

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 // body parser configuration
 app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (request, response, next) => {
   response.json({ message: "Hey! This is your server response!" });
